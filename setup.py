c3711d1bfbfb34f6c87afa8b2bd33b4f21bdc708
Dorian Jaminais <dorian@jaminais.fr>
2019-08-31 21:17:38 -0700

    Package and distribute

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="argparse_logging",
+    version="0.1",
+    author="Dorian Jaminais",
+    author_email="argparse_logging@jaminais.fr",
+    description="This is a simple library to configure logging from command "
+    "line argument when using argparse.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nanassito/argparse_logging",
+    py_modules=["argparse_logging"],
+    test_suite="test_all",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Public Domain",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
