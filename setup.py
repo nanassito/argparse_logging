2162a764f6b66bad22bb73d1d2093e66f5877982
Dorian Jaminais <dorian@jaminais.fr>
2019-12-22 12:13:37 +0100

    Upgrade to py3.8

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="argparse_logging",
-    version="0.3",
+    version="0.4",
     author="Dorian Jaminais",
     author_email="argparse_logging@jaminais.fr",
     description="This is a simple library to configure logging from command "
